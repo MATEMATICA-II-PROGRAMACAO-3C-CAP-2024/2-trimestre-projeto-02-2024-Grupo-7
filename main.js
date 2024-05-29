6d08cd72c69a81c5874a132c74a8ab40f48db55c
Magihnx <127496945+Magihnx@users.noreply.github.com>
2024-05-29 09:16:22 -0300

    teste

--- main.js
+++ main.js
@@ -3,16 +3,31 @@ const numeroSenha= document.querySelector('.parametro-senha-_texto');
 let tamanhoSenha=12;
 botoes[0].onclick= diminuiTamanho;
 botoes[1].onclick= aumentaTamanho;
-console.log(botoes);
+//código omitido
+const campoSenha= document.querySelector('#campo-senha');
+const letrasMaiusculas= "ABCDEFGHIJKLMNOPQRSTUVXYWZ";
+function geraSenha (){
+    let senha= '';
+    for (let i = 0; i < tamanhoSenha;i++) {
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiusculas[numeroAleatorio];
+    console.log(letrasMaiusculas[numeroAleatorio]);
+    }
+    campoSenha.value = senha;
+}
+campoSenha.value = letrasMaiusculas;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1)
 tamanhoSenha = tamanhoSenha--;
-numeroSenha.textContent= tamanhoSenha
+numeroSenha.textContent= tamanhoSenha;
+geraSenha
 }
 
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
 tamanhoSenha = tamanhoSenha++;}
 numeroSenha.textContent = tamanhoSenha;
+geraSenha;
 }
