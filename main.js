cf87a4d53992c36d59a3bf52fe6fac58340f4dc9
Magihnx <127496945+Magihnx@users.noreply.github.com>
2024-05-29 08:55:42 -0300

    rascunho 5

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const numeroSenha= document.querySelector('.parametro-senha__botao'); numeroSenha.textContent= 5;
+const numeroSenha= document.querySelector('.parametro-senha__botao'); 
+const numeroSenha= document.querySelector('.parametro-senha-_texto');
+let tamanhoSenha=12;
+botoes[0].onclick= diminuiTamanho;
+botoes[1].onclick= aumentaTamanho;
+console.log(botoes);
+
+function diminuiTamanho(){
+tamanhoSenha = tamanhoSenha - 1;
+numeroSenha.textContent= tamanhoSenha
+}
+
+function aumentaTamanho(){
+tamanhoSenha = tamanhoSenha + 1;
+numeroSenha.textContent = tamanhoSenha;
+}
